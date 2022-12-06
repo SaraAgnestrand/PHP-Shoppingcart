5952fb6a338ad6b8039f964ae76c7931d8ef53d2
Sara Agnestrand <sara@agnestrand.net>
2022-12-06 12:25:55 +0100

    added comments

--- ShoppingCart PHP OOP/CartItem.php
+++ ShoppingCart PHP OOP/CartItem.php
@@ -1,44 +1,38 @@
 <?php
-
-
 class CartItem
 {
     private $product;
     private int $quantity;
 
-    // TODO Skriv en konstruktor som sätter alla properties
+    // Konstruktor för alla properties
 
- public function __construct($product, $quantity) {
+    public function __construct($product, $quantity) 
+    {
         $this->product = $product;
         $this->quantity = $quantity;
-        
     }
-    // TODO Skriv getters för alla properties
-public function getProduct()
-{
-    return $this->product;
-}
+    //Getters för alla properties
+    public function getProduct()
+    {
+        return $this->product;
+    }
 
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
 
-public function getQuantity()
-{
-    return $this->quantity;
-}
-
-    //VG: Skall utöka antalet på ett cartItem med 1. 
-    //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
-    public function increaseQuantity(){
+    /*Metod för att öka på antal av ett cartItem med 1, 
+    det går ej att utöka så att antalet överstiger produktens inStock.*/
+    public function increaseQuantity()
+    {
 
         if ($this->quantity < $this->product->getInStock()){    
             $this->quantity++;
 
         } else {
             echo "Kan inte lägga till. Slut i lager";
-
-           
-
         }
-
     }
 }
 ?>
